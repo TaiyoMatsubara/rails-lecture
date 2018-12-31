8f10775c6ecf6c9553db07686802901847628d01
Taiyo Matsubara <taiyo.matsubara@vitalize.co.jp>
2018-12-31 09:13:17 +0000

    ユーザー認証設定途中まで

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_11_110911) do
+ActiveRecord::Schema.define(version: 2018_12_29_025515) do
 
   create_table "board_tag_relations", force: :cascade do |t|
     t.integer "board_id"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 2018_11_11_110911) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
