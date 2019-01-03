32539d2d73dca859fab4c5fcd31c341291995032
Taiyo Matsubara <taiyo.matsubara@vitalize.co.jp>
2019-01-03 08:58:05 +0000

    一旦コミット

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   get 'mypage', to: 'users#me'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destory'
-  get 'sessions/create'
-  get 'sessions/destory'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   resources :users, only: %i[new create]
